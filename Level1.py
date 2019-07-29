95f107e8151af38a5d1f1ad575a46ed3620cac2c
SarahRosenfeld <47671508+SarahRosenfeld@users.noreply.github.com>
2019-07-29 18:18:22 -0400

    Add files via upload

--- Level1.py
+++ Level1.py
@@ -11,6 +11,7 @@ height = 700
 
 background = (102, 178, 255)
 
+jump_noise = pygame.mixer.Sound('Sound Effects/smb_jump-small.wav')
 pygame.mixer.music.load('Sound Effects/Main Theme.mp3')
 pygame.mixer.music.play(-1)
 
@@ -30,6 +31,8 @@ hill = pygame.image.load("Images/Hill.png")
 pipe = pygame.image.load("Images/pipe.png")
 clouds = pygame.image.load("Images/Clouds.png")
 mystery = pygame.image.load("Images/Mystery Block.png")
+flag = pygame.image.load("Images/Flagpole.png")
+castle = pygame.image.load("Images/Castle.png")
 
 def coins(x,y):
     screen.blit(coinImg,(x,y))
@@ -60,43 +63,47 @@ jumpCount = 0
 backSpot = 0
 
 class User(pygame.sprite.Sprite):
+    height: int
+
     def __init__(self, x, y, image):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image)
-        self.x = x
-        self.y = y
+        self.x: int = x
+        self.y: int  = y
         self.height = 65
         self.width = 49
         self.hitbox = (self.x, self.y, 49, 65)
-    def printUser(self, x, y, screen):
-        screen.blit(self.image, (x, y))
-        self.hitbox = (x, y, 49, 65)
-        #pygame.draw.rect(screen, (255,0,0), self.hitbox, 2)
-    def collisio(self, x, y, width, height):
-        #print(x, x + width, self.hitbox[0], self.hitbox[0] + self.x )
-        #print(y, y + height, self.hitbox[1], self.hitbox[1] + self.y)
-        if self.hitbox[0] <= x <= self.hitbox[0] + self.x or self.hitbox[0] <= x + width <= self.hitbox[0] + self.x:
-            print("x hit")
-            print(self.hitbox[1], " ", self.hitbox[1] - self.height, y, y-height)
-            if self.hitbox[1] - self.height <= y <= self.hitbox[1] or self.hitbox[1] - self.height <= y + height <= self.hitbox[1]:
-                print("hit obstacle")
-                return True
-        else:
-            return False
+        self.bottom = self.y - 65
+        self.center = self.x + 25
+    def printUser(self, screen):
+        screen.blit(self.image, (mario.x, mario.y))
+        self.hitbox = (mario.x, mario.y, 49, 65)
     def collision(self, obs : tuple):
-        #print(x, x + width, self.hitbox[0], self.hitbox[0] + self.x)
-        #print(y, y + height, self.hitbox[1], self.hitbox[1] + self.y)
         if self.hitbox[0] <= obs[0] <= self.hitbox[0] + self.x or self.hitbox[0] <= obs[0] + obs[2] <= self.hitbox[0] + self.x:
-            #print("x:", self.hitbox[0], self.hitbox[0] + self.width, obs[0], obs[0] + obs[2])
-            #print("y:", self.hitbox[1], self.hitbox[1] + self.height, obs[1], obs[1] + obs[3])
             if self.hitbox[1] + self.height >= obs[1] >= self.hitbox[1] or self.hitbox[1] + self.height >= obs[1] + obs[3] >= self.hitbox[1]:
-                #print("hit obstacle")
-                if self.hitbox[1] + self.height >= obs[1]  >= self.hitbox[1]:
+                # check if it is on top
+                if self.hitbox[1] + self.height >= obs[1] >= self.hitbox[1]:
                     return 1
                 return 2
 
         else:
             return 3
+    def top(self):
+        return self.y
+    def bottom(self):
+        return self.y + self.height
+    def left(self):
+        return self.x
+    def right(self):
+        return self.x + self.width
+    def center(self):
+        return self.x + 25
+    def update(self, x : int,y : int ):
+        self.x = x
+        self.y = y
+        self.bottom = y + 65
+        self.center = x + 25
+
 
 
 class obstacle():
@@ -107,22 +114,32 @@ class obstacle():
         self.x : int= x
         self.y : int= y
     def upObs(self, back):
-        #screen.blit(image, (x, y))
         self.hitbox = (self.x + back, self.y, self.wid, self.h)
     def box(self):
         return self.hitbox
     def top(self):
         return self.y
+    def bottom(self):
+        return self.y + self.h
+    def left(self):
+        return self.x
+    def right(self):
+        return self.x + self.wid
+    def updaate(self, x, y):
+        self.x: int = x
+        self.y: int = y
+    def changeY(self, y):
+        self.y = y
 
 def print_Back(x, height):
     screen.fill(backColor)
-    for i in range(0, 59):
+    for i in range(0, 79):
         if i == 12 or i == 24 or i == 42:
             screen.blit(bush,(54 * i + x, height - 2 * 54 - 103))
         if i == 0 or i == 16 or i == 48:
             screen.blit(hill,(54 * i + x, height - 2 * 54 - 128))
         if i == 29 or i == 38 or i == 46 or i == 57:
-            screen.blit(pipe,(54 * i + x, height - 2 * 54 - 189))
+            screen.blit(pipe,(54 * i + x, height - 2 * 54 - 120))
         if i == 16 or i == 20 or i == 22:
             screen.blit(mystery,(54 * i + x, height - 2 * 54 - 150))
         if i == 19 or i == 21 or i == 23:
@@ -135,13 +152,60 @@ def print_Back(x, height):
             screen.blit(clouds, (54 * i + x, height - 2 * 54 - 400))
         if i == 5 or i == 40:
             screen.blit(clouds, (54 * i + x, height - 2 * 54 - 450))
+        if i == 63:
+            screen.blit(flag, (54 * i + x, height - 2 * 54 - 350))
+        if i == 65:
+            screen.blit(castle, (54 * i + x, height - 2 * 54 - 376))
         screen.blit(bottom_brick, (54 * i + x, height - 54))
         screen.blit(bottom_brick, (54 * i + x, height - 2 * 54))
 
+def checkPlatforms(mario,x, jumping):
+    if (54 * 19 + x) <= mario.x <= (54 * 24 + x):
+        print(700 - 54 * 2 - 150, mario.bottom, 700 - 54 * 2 - 150 - 65)
+        print("x", 54*19+x, mario.x, 54*25+x)
+        if (442 + 65) >= mario.y > (377 + 65):
+            # if hits bottom
+            # FIX THIS
+            mario.y += 30
+        elif (442) >= mario.y >= (377):
+            # if on top of platform
+            print("here", mario.x)
+            mario.y = 377  # 700 - 2 * 54 - 150 - 65
+        elif mario.x < 377 + 54:
+            #if below
+            mario.y = 700 - 54 * 2 - 65
+    elif (54 * 16 + x) <= mario.center <= (54 * 17 + x):
+        if (442 + 65) >= mario.y > (377 + 65):
+            mario.y += 30
+        elif (442) >= mario.y >= (377):
+            print("here", mario.x)
+            mario.y = 377  # 700 - 2 * 54 - 150 - 65
+        elif mario.x < 377 + 54:
+            mario.y = 700 - 54 * 2 - 65
+    elif 54 * 29 + x <= mario.x <= 54 * 29 + 114 + x and (442+ 54) >= mario.y >= (377 + 54):
+            mario.y = 377 +54
+    elif 54 * 38 + x <= mario.x <= 54 * 38 + 114 + x and (442 + 54) >= mario.y >= (377 + 54):
+            mario.y = 377 + 54
+    elif 54 * 46 + x <= mario.x <= 54 * 46 + 114 + x and (442 + 54) >= mario.y >= (377 + 54):
+            mario.y = 377 + 54
+    elif 54 * 57 + x <= mario.x <= 54 * 57 + 114 + x and (442 + 54) >= mario.y >= (377 + 54):
+            mario.y = 377 + 54
+    elif not jumping and mario.y < 700 - 54 * 2 - 65:
+        mario.y += 30
+    elif not jumping and mario.y >= 700 - 54 * 2 - 65:
+        mario.y = 700 - 54 * 2 - 65
+    elif not jumping:
+        if mario.y == (700-54 * 2 - 65):
+            mario.y = 700 - 54*2 - 65
+        else:
+            mario.y += 30
+
 
 marioImg = "Images/mario.png"
-mario = User(x, y, marioImg)
-platform = obstacle(54 * 19 + x, height - 2 * 54 - 150, 54 * 6, 54)
+mario = User(0, 700 - 54*2 - 65, marioImg)
+platform = obstacle(54 * 20 + x, height - 2 * 54 - 150, 54 * 6 , 54)
+box = obstacle(54 * 18 + x, height - 2 * 54 - 150, 54, 54)
+ground = obstacle(0, height - 54*2, 54*60, 54*2)
 
 while not crashed:
     pygame.time.delay(10)
@@ -153,40 +217,80 @@ while not crashed:
 
         if event.type == pygame.KEYDOWN:
             if usrKey[pygame.K_s]:
-                if usrKey[pygame.K_l] and usrKey[pygame.K_s]:
-                    if x < width / 2 - speed - marWidth:
-                        x += speed * 2
+                if usrKey[pygame.K_k] and usrKey[pygame.K_s]:
+                    if mario.x < width / 2 - speed - marWidth:
+                        if 54 * 29 + backSpot - 45 <= mario.x <= 54 * 29 + backSpot and not jumping:
+                            pass
+                        elif 54 * 38 + backSpot - 45 <= mario.x <= 54 * 38 + backSpot and not jumping:
+                            pass
+                        elif 54 * 46 + backSpot - 45 <= mario.x <= 54 * 46 + backSpot and not jumping:
+                            pass
+                        elif 54 * 57 + backSpot - 45 <= mario.x <= 54 * 57 + backSpot and not jumping:
+                            pass
+                        else:
+                            mario.x += speed * 2
                     else:
-                        rightfast = True
-                elif x < width / 2 - speed - marWidth:
-                    x += speed
+                        if 54 * 29 + backSpot - 45 <= mario.x <= 54 * 29 + backSpot and not jumping:
+                            pass
+                        elif 54 * 38 + backSpot - 45 <= mario.x <= 54 * 38 + backSpot and not jumping:
+                            pass
+                        elif 54 * 46 + backSpot - 45 <= mario.x <= 54 * 46 + backSpot and not jumping:
+                            pass
+                        elif 54 * 57 + backSpot - 45 <= mario.x <= 54 * 57 + backSpot and not jumping:
+                            pass
+                        else:
+                            rightfast = True
+                elif mario.x < width / 2 - speed - marWidth:
+                    if 54 * 29 + backSpot - 45 <= mario.x <= 54 * 29 + backSpot and not jumping:
+                        pass
+                    elif 54 * 38 + backSpot - 45 <= mario.x <= 54 * 38 + backSpot and not jumping:
+                        pass
+                    elif 54 * 46 + backSpot - 45 <= mario.x <= 54 * 46 + backSpot and not jumping:
+                        pass
+                    elif 54 * 57 + backSpot - 45 <= mario.x <= 54 * 57 + backSpot and not jumping:
+                            pass
+                    else:
+                        mario.x += speed
+
                 else:
-                    rightmove = True
+                    if 54 * 29 + backSpot - 45 <= mario.x <= 54 * 29 + backSpot and not jumping:
+                        pass
+                    elif 54 * 38 + backSpot - 45 <= mario.x <= 54 * 38 + backSpot and not jumping:
+                        pass
+                    elif 54 * 46 + backSpot - 45 <= mario.x <= 54 * 46 + backSpot and not jumping:
+                        pass
+                    elif 54 * 57 + backSpot - 45 <= mario.x <= 54 * 57 + backSpot and not jumping:
+                        pass
+                    else:
+                        rightmove = True
             elif usrKey[pygame.K_a] and x > speed:
-                x -= speed
-                if usrKey[pygame.K_l] and usrKey[pygame.K_a]:
-                    x -= speed
+                mario.x -= speed
+                if usrKey[pygame.K_k] and usrKey[pygame.K_a]:
+                    mario.x -= speed
             if not jumping:
-                if (usrKey[pygame.K_k] and usrKey[pygame.K_a]) or (usrKey[pygame.K_k] and usrKey[pygame.K_s]) or usrKey[pygame.K_k]:
+                if (usrKey[pygame.K_l] and usrKey[pygame.K_a]) or (usrKey[pygame.K_l] and usrKey[pygame.K_s]) or usrKey[pygame.K_l]:
                     jumping = True
-                    jumpCount = 7
+                    jumpCount = 8
+                    pygame.mixer.music.pause()
+                    jump_noise.play()
+                    pygame.mixer.music.unpause()
             else:
                 if jumpCount >= 0:
-                    y -= jumpCount ** 2
+                    mario.y -= jumpCount ** 2
                     jumpCount-= 1
-                elif jumpCount >= -7:
-                    y += jumpCount ** 2
+                elif jumpCount >= -8:
+                    mario.y += jumpCount ** 2
                     jumpCount -= 1
-                elif jumpCount == -8:
+                elif jumpCount == -9:
                     jumping = False
             if falling:
-                if fallCount >= -7:
-                    y += fallCount ** 2
+                if fallCount >= -8:
+                    mario.y += fallCount ** 2
                     fallCount -= 1
                 elif fallCount == -9:
                     falling = False
                     onSurface = False
-                    y = height - 2 * 54 - 65
+                    mario.y = height - 2 * 54 - 65
 
 
         if rightmove:
@@ -197,20 +301,16 @@ while not crashed:
 
         print_Back(backSpot, height)
         platform.upObs(backSpot)
-        #User.printUser(mario, x, y, screen)
-        if User.collision(mario, platform.box()) == 1:
-            y = platform.top() - marHeight
-            jumping = False
-            #crashed = True
-            print("HIT")
-            onSurface = True
-        elif User.collision(mario, platform.box()) == 3 and onSurface:
-            falling = True
-            #fallCount = 0
+        mario.update(mario.x, mario.y)
 
-        User.printUser(mario, x, y, screen)
-        rightmove = False
-        rightfast = False
+        checkPlatforms(mario, backSpot, jumping)
+
+
+
+
+    User.printUser(mario, screen)
+    rightmove = False
+    rightfast = False
 
 
 
@@ -220,4 +320,3 @@ while not crashed:
 
 pygame.quit()
 quit()
-
